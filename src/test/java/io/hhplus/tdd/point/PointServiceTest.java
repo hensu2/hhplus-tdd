8895876a5ae8a96fdf980f68232e69276160e191
HyeonSuJeong <hensu3846@gmail.com>
2025-10-22 14:48:02 +0900

    test : 포인트 조회 후 데이터 없을 시 테스트 추가

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -52,4 +52,31 @@ public class PointServiceTest {
         assertThat(result.id()).isEqualTo(userId); // 같은지
         assertThat(result.point()).isEqualTo(1000L); // 같은지
     }
+
+    @Test
+    @DisplayName("포인트 조회 후 없을 시 포인트가 0 인 UserPoint객체 리턴")
+    void getUserPoint_() {
+        /*
+            테스트 사유
+           UserPointTable selectById 리턴값이  map.getOrDefault(id, UserPoint.empty(id))
+           이라서 테스트
+         */
+
+        // given
+        // 파라미터 설정
+        long userId = 999L;
+
+        when(userPointTable.selectById(userId)).thenReturn(UserPoint.empty(userId));
+        // when(userPointTable.selectById(userId)) 실행 시
+        // thenReturn(UserPoint.empty(userId));  UserPoint.empty(userId) 리턴
+
+        // when 실행
+        UserPoint result = pointService.getUserPoint(userId);
+        // pointService.getUserPoint 함수가 없어서 실패
+
+        // then
+        assertThat(result).isNotNull(); // null이 아닌지
+        assertThat(result.id()).isEqualTo(userId); // 같은지
+        assertThat(result.point()).isEqualTo(0); // 같은지
+    }
 }
