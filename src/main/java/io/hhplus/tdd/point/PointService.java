f9014eb16f92aba11025e81b59ab5de068411c23
HyeonSuJeong <hensu3846@gmail.com>
2025-10-22 14:16:18 +0900

    feat : 포인트 조회 TDD Green UserPointTable 연결 및 selectById 연결

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,7 +1,17 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
+    private final UserPointTable userPointTable;
+
+    public PointService(UserPointTable userPointTable) {
+        this.userPointTable = userPointTable;
+    }
+
+    public UserPoint getUserPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
 }
